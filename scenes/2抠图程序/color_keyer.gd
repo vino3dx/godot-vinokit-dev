edfdc8f254852a5f3e898c4535201952814879bd
vino <vino3dx@gmail.com>
2026-09-02 17:00:23 +0800

    initial commit

---
+++ scenes/2抠图程序/color_keyer.gd
@@ -0,0 +1,6 @@
+extends Control
+
+@onready var file_dialog: FileDialog = $FileDialog
+@onready var color_picker_button: ColorPickerButton = $ColorPickerButton
+@onready var h_slider: HSlider = $HSlider
+@onready var texture_rect: TextureRect = $TextureRect
